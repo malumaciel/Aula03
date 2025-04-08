7d8c0e1c8c67794cb38193c2f05716d3a1013926
unknown <maaluu740@gmail.com>
2025-04-08 16:57:55 -0300

    Adicionando todos

---
+++ Range.py
@@ -0,0 +1,8 @@
+soma=0
+for j in range(10):
+    num = int(input("Digite um número:"))
+    soma = soma + num
+print(soma)
+
+
+
---
+++ SOMA.py
@@ -0,0 +1,4 @@
+num = int (input("Digite seu número: "))
+for j in range (1,11,1):
+    print(f"{num} X {j} = {num * j}")
+
